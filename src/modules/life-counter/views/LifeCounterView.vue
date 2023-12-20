a392d0d4a0bfe1f650ca1d671261787a291c4341
Lucas Gobbi <llcostagobbi@gmail.com>
2023-12-20 17:14:14 -0300

    feat: Implementing Life Counter view

---
+++ src/modules/life-counter/views/LifeCounterView.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+import LifeCalculator from '../components/LifeCalculator/LifeCalculator.vue'
+import TurnManager from '../components/TurnManager.vue'
+</script>
+
+<template>
+  <div class="h-screen w-screen overflow-hidden bg-black font-lemon text-white">
+    <TurnManager />
+    <LifeCalculator />
+  </div>
+</template>
