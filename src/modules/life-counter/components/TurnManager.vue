9b8b31a7d1b0781e39619356711e6bd7353139da
Lucas Gobbi <llcostagobbi@gmail.com>
2023-12-20 17:13:18 -0300

    feat: Implementing Turn Manager Component

---
+++ src/modules/life-counter/components/TurnManager.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import { useTurnStore } from '@/modules/life-counter/stores/TurnStore'
+
+const turnStore = useTurnStore()
+</script>
+
+<template>
+  <h2>Turno</h2>
+  <div class="flex gap-8">
+    <button @click="turnStore.dec">-</button>
+    <p>{{ turnStore.turn }}</p>
+    <button @click="turnStore.inc">+</button>
+  </div>
+
+  <h2>Mana</h2>
+  <p>{{ turnStore.mana }}</p>
+</template>
