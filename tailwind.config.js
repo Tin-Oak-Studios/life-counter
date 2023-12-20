6e630b1791579874e21ea1c1d270571605761656
Lucas Gobbi <llcostagobbi@gmail.com>
2023-12-20 17:12:54 -0300

    fix: Correcting config files

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+import plugin from "tailwindcss/plugin.js"
+
+export default {
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      },
+      textShadow: {
+        sm: '0 1px 2px var(--tw-shadow-color)',
+        DEFAULT: '0 2px 4px var(--tw-shadow-color)',
+        lg: '0 8px 16px var(--tw-shadow-color)',
+      },
+    },
+    fontFamily: {
+      'lemon': ['"Lemon/Milk"'],
+    },
+  },
+  plugins: [
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          'text-shadow': (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme('textShadow') }
+      )
+    }),
+  ],
+  purge: ["./index.html",'./src/**/*.{vue,js,ts}'], //for unused css
+  variants: {
+    extend: {},
+  },
+
+  darkmode: false, // or 'media' or 'class'
+}
