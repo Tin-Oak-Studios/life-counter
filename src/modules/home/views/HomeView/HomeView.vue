42a3b0708c7592e1ebceb10c7b13147a7e30d587
Elian Batista <elianbatista2000@gmail.com>
2023-12-28 19:59:13 -0300

    feat: create HomeView

---
+++ src/modules/home/views/HomeView/HomeView.vue
@@ -0,0 +1,77 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div :class="$style.container">
+    <div :class="$style.content">
+      <img :class="$style.image" src="@/assets/images/lifecounter/icone.png">
+      <nav :class="$style.nav">
+        <div :class="[$style.link, $style.active]">
+          <router-link to="/">
+            Home
+          </router-link>
+        </div>
+        <div :class="$style.link">
+          <router-link to="/life-counter">
+            Life Counter
+          </router-link>
+        </div>
+        <div :class="$style.link">
+          <router-link to="/card-creator">
+            Card Creator
+          </router-link>
+        </div>
+      </nav>
+    </div>
+    <div :class="$style.copyright">
+      Powered by TinOak Studios
+    </div>
+  </div>
+</template>
+
+<style lang="scss" module>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 8px;
+  height: 100vh;
+  background-color: #222;
+
+  .content {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    flex: 1;
+
+    .image {
+      width: 200px;
+      margin-bottom: 16px;
+    }
+
+    .nav {
+      display: flex;
+      gap: 16px;
+
+      .link {
+        color: #fff;
+        cursor: pointer;
+
+        &.active {
+          border-bottom: 1px solid #fff;
+        }
+
+        &:hover {
+          border-bottom: 1px solid #fff;
+        }
+      }
+    }
+  }
+
+  .copyright {
+    color: #fff;
+  }
+}
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { HomeView } from '@/modules/home/views'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,16 +10,8 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    },
-    {
-      path: '/lifecounter',
-      name: 'lifecounter',
+      path: '/life-counter',
+      name: 'lifeCounter',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
